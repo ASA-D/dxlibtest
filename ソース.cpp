65b01b2ec8e31c225c4442a353ee7b74c312b662
ASA-D <fall-DESK@outlook.com>
2021-07-11 11:16:42 +0900

    プロジェクト ファイルを追加します。

---
+++ ソース.cpp
@@ -0,0 +1,20 @@
+#include "DxLib.h"
+#include <string>
+
+// プログラムは WinMain から始まります
+using namespace std;
+int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nCmdShow)
+{
+	if (DxLib_Init() == -1)		// ＤＸライブラリ初期化処理
+	{
+		return -1;			// エラーが起きたら直ちに終了
+	}
+
+	DrawString(200, 200, "we", 255, 255);
+
+	WaitKey();				// キー入力待ち
+
+	DxLib_End();				// ＤＸライブラリ使用の終了処理
+
+	return 0;				// ソフトの終了 
+}
